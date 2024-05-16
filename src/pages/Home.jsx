10c0be31eb44cbbed7850b6e678c6a62e5b61008
Saliksheikh19 <salik44sheikh@gmail.com>
2024-05-16 16:07:43 -0700

    nav,sidebar,login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 
 function Home() {
   return (
-    <div>Home</div>
+  <div>
+      <Navbar/>
+    <div className="flex">
+    <Sidebar />
+    <main className="flex-1 bg-gray-100 h-screen p-4">
+      <h1>Main Content</h1>
+    </main>
+  </div>
+  </div>
   )
 }
 
