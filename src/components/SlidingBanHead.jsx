0e50e447db6b35909b388f56ca498b804b1749b5
Saliksheikh19 <salik44sheikh@gmail.com>
2024-05-20 13:47:18 -0700

    add service and add add product

---
+++ src/components/SlidingBanHead.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { IoAdd } from "react-icons/io5";
+const SlidingBanHead= () => {
+  return (
+    <div>
+        <div className='font-bold text-xl text-slate-600 p-5 bg-white flex mb-8'> < IoAdd className='text-md'/><h1>Add Sliding Banners</h1></div>
+        
+    </div>
+  )
+}
+
+export default SlidingBanHead
---
+++ src/pages/AddSlidingBan.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SlidingBanTiny from '../components/SlidingBanTiny'
+import SlidingBanHead from '../components/SlidingBanHead'
+
+function AddSlidingBan() {
+  return (
+    <div>
+        <SlidingBanHead/>
+        <SlidingBanTiny/>
+    </div>
+  )
+}
+
+export default AddSlidingBan
