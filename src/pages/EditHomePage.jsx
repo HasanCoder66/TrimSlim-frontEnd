0820fdb8b5df8d51e2e1ca404b5bd85da6852161
HamzaBashir123 <hamzabashir1268@gmail.com>
2024-05-20 15:30:12 -0700

    AllEdit pages

---
+++ src/pages/EditHomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import EditHomePageHeader from '../components/EditHomePageHeader'
+import EditHomePageTable from '../components/EditHomePageTable'
+
+const EditHomePage = () => {
+  return (
+    <div>
+        <EditHomePageHeader heading='Edit Home Page'/>
+        <EditHomePageTable/>
+    </div>
+  )
+}
+
+export default EditHomePage
---
+++ src/pages/EditOurTeamPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import EditHomePageHeader from '../components/EditHomePageHeader'
+import EditServicesPageTable from '../components/EditServicesPageTable'
+
+const EditOurTeamPage = () => {
+  return (
+    <div>
+        <EditHomePageHeader heading='Edit Our Team Page'/>
+       
+       
+    </div>
+  )
+}
+
+export default EditOurTeamPage
